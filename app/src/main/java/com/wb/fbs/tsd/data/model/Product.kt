f6a975bbfc0ad64802c072b4db6b569558438057
afgsasdasfg <misha.cold@yandex.ru>
2026-08-12 10:59:47 +0300

    WE ARE F*CKING ON FIRE

---
+++ app/src/main/java/com/wb/fbs/tsd/data/model/Product.kt
@@ -0,0 +1,14 @@
+package com.wb.fbs.tsd.data.model
+
+data class Product(
+    val id: String,
+    val article: String,
+    val name: String,
+    val color: String,
+    val size: String,
+    val barcode: String,
+    val requiresMarking: Boolean = false,
+    val quantityInStock: Int = 0,
+    val kizCodes: List<String> = emptyList(),
+    val receivedAt: Long = System.currentTimeMillis()
+)
